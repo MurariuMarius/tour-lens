7ff8e9231712eb0bd010c096d415b3ae2c61d21c
MurariuMarius <mam.murariu@gmail.com>
2024-12-06 15:58:09 +0200

    Added firebase function for processing destinations

---
+++ backend/firebase/functions/destination.js
@@ -0,0 +1,61 @@
+const { HttpsError, onCall } = require("firebase-functions/v2/https");
+const logger = require("firebase-functions/logger");
+
+const FormData = require("form-data");
+const fetch = require("node-fetch");
+
+const externalEndpointUrl = "https://tour-lens-ml-455665426558.us-central1.run.app/model";
+
+exports.createDestination = onCall(
+  {
+    timeoutSeconds: 540,
+    memory: "2GiB",
+    cpu: 4,
+  },
+  async (request) => {
+
+    try {
+        const destination = request.data.destination;
+        console.log("Received Destination Data:", destination);
+
+        const destinationId = destination.id;
+        const attractions = destination.attractions;
+
+        const formData = new FormData();
+
+        attractions.forEach(attraction => {
+            const attractionLabel = attraction.label;
+            const pictures = attraction.pictures;
+
+            pictures.forEach((picture, index) => {
+                const imageBuffer = Buffer.from(picture, 'base64');
+                const filename = `${attractionLabel}_${destinationId}_${index}.jpg`;
+                formData.append('images', imageBuffer, { filename });
+            });
+        });
+
+    const fetchResponse = await fetch(externalEndpointUrl, {
+      method: 'POST',
+      headers: {
+        ...formData.getHeaders()
+      },
+      body: formData
+    });
+
+    if (!fetchResponse.ok) {
+      const errorBody = await fetchResponse.text();
+      console.error("Error sending images:", fetchResponse.status, errorBody);
+      throw new HttpsError('internal', `Failed to send images: ${errorBody}`);
+    }
+
+    const responseData = await fetchResponse.json();
+
+    
+    console.log("Images sent successfully:", responseData);
+    return { message: "Images sent successfully", data: responseData };
+
+  } catch (error) {
+    console.error("Error processing request:", error);
+    throw new HttpsError('unknown', `Failed to process request: ${error.message}`, error);
+  }
+});
