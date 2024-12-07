9fb27f322c912eb15594aa92d2c1e89198fd7c09
MurariuMarius <mam.murariu@gmail.com>
2024-12-07 18:33:11 +0200

    Added endpoint to get available models

--- backend/ml/app.py
+++ backend/ml/app.py
@@ -3,6 +3,9 @@ from model import predict, create_model, delete_model
 from persistence import check_model
 
 import traceback, uuid
+import os
+
+import config
 
 app = Flask(__name__)
 
@@ -13,9 +16,14 @@ def predict_endpoint():
         img_bytes = request.get_data()
         
         model_id = request.args.get("id")
-        
+                
         if not img_bytes:
             return jsonify({'error': 'No image data received. Please send image data in the request body.'}), 400
+        
+        if not model_id:
+            return jsonify({'error': 'Must specify model id'}), 400
+        
+        print(f'Predicting for model {model_id}...')
         
         try:
             check_model(model_id)
@@ -68,6 +76,24 @@ def delete():
         return jsonify({'error': 'Could not delete model'}), 500
         
     return jsonify({}), 204
+
+
+@app.route('/model', methods=['GET'])
+def get_models():
+    try:
+        models = os.listdir(config.model_base_path)
+        print(models)
+        
+        try:
+            models = filter(lambda model : check_model(model))
+        except LookupError as e:
+            print(e)
+        
+        return jsonify({'models': models}), 200
+    except Exception as e:
+        print(e)
+        traceback.print_exc()
+        return jsonify({'error': 'Could not fetch models'}), 500
 
     
 if __name__ == '__main__':
